58cf7c8b9b97b6cafbb1c14b5c2773d8bc43c879
kald-a <54445891+kald-a@users.noreply.github.com>
2019-09-18 09:34:56 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## converts matrix so that calculations are cacheable
+## returns a list of functions to set the matrix, get the matrix, set the inverse
+##allows you to check whether an inverse has already been computed
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -20,7 +19,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##checks if inverse for that input has already been calculated (by seeing if it is still NULL)
+##if already calculated, prints message and returns the cached inverse
+##otherwise it retrieves the matrix, solves the inverse, sets the inverse value for that particular input, and returns it
 
 cacheSolve <- function(x, ...) {
   i <- x$getinverse()
